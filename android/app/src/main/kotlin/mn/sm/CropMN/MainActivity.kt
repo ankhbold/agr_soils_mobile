46b4bc432418b8232c7f5af0cc3390682a80bb77
Unubold <unuu3167@gmail.com>
2023-05-06 11:48:16 +0800

    update

---
+++ android/app/src/main/kotlin/mn/sm/CropMN/MainActivity.kt
@@ -0,0 +1,6 @@
+package mn.sm.CropMN
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- android/app/src/main/mn/sm/CropMn/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package mn.sm.CropMn
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
